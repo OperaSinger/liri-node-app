7bf4e46cdfe29c29273c840fc9ea4a09893b9e1f
Nigel Tufnel <tenorideout@gmail.com>
2018-07-12 23:42:50 -0400

    incomplete, but will finish

--- liri.js
+++ liri.js
@@ -1,19 +1,22 @@
 // Include the dotenv package:
 require("dotenv").config();
+
 // Include the request npm package:
 var request = require("request");
 
 var doThis = process.argv[2];
 // console.log(process.env.OMDB_ID)
-var word = ("");
+// var word = ("");
 
-var string = function() {
-    for (i = 3; i < (process.argv.length - 2); i++) {
-        var word = `${word}+${process.argv[i]} `;
-    };
-}
+//     for (i = 3; i < process.argv.length; i++) {
+//         word = `${word} ${process.argv[i]}`;
+//     }; OR:::::
+// var string = word.split(" ").join("+")
+// console.log(string);
 
-console.log(string);
+// slice will take in a string starting at index 3 until the end of input and insert a + sign for searches
+
+var term = process.argv.slice(3).join("+");
 
 switch (doThis) {
     case "my-tweets":
@@ -30,31 +33,105 @@ switch (doThis) {
         break;
     default:
         console.log("You should have chosen more wisely");
-}
+};
 
 function twitter() {
 
-}
+    var twitterURLQuery = "statuses/user_timeline"
 
-function spotify() {
+    var params = { user_id: "1017118758001930253", count: 20 }
 
-}
+    client.get(twitterURLQuery, params, function (err, tweets) {
+
+        if (!err) {
+            for (let i = 0; i < tweets.length; i++) {
+                console.log(`You tweeted "${tweets[i].text}" on ${tweets[i].created_at}\n`);
+            }
+        };
+        if (err) {
+            console.log(err);
+        };
+    });
+};
+
+// function spotify() {
+    // var spotify = new Spotify(keys.spotify);
+    // var client = new Twitter(keys.twitter);
+//     // if no song entered, automatically put up info for
+//     // The Sign by Ace of Base:
+
+//     if (term == "") {
+//         term = "The Sign";
+//     // request the Spotify API:
+//     request()
+
+//     client.get('statuses/user_timeline', params , function (err,data,response) {
+//         if (err) {
+//           return console.log('Error occurred: ' + err);
+//         }
+
+//     }
+// }
 
 function movie() {
-// Then run a request to the OMDB API with the movie specified
-request(`http://www.omdbapi.com/?t=${string}&y=&plot=short&apikey=${process.env.OMDB_ID}`, function(error, response, body) {
 
-  // If the request is successful (i.e. if the response status code is 200)
-  if (!error && response.statusCode === 200) {
+    if (term === "") {
+        term = "Mr. Nobody";
 
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    // console.log(JSON.parse(body));
-  }
-});
-}
+        // Then run a request to the OMDB API with the movie specified
+        request(`http://www.omdbapi.com/?t=${term}&y=&plot=short&apikey=${process.env.OMDB_ID}`, function (error, response, body) {
 
-function randomDo() {
+            // If the request is successful (i.e. if the response status code is 200)
+            if (!error && response.statusCode === 200) {
 
-}
+                console.log(`\nTitle of movie: ${JSON.parse(body).Title}`);
+                console.log(`\nRelease Year: ${JSON.parse(body).Year}`);
+                console.log(`\nIMDB Rating: ${JSON.parse(body).Ratings[0].Value}`);
+                console.log(`\nRotten Tomatoes Rating: ${JSON.parse(body).Ratings[2].Value}`);
+                console.log(`\nCountry where produced: ${JSON.parse(body).Country}`);
+                console.log(`\nMovie Language: ${JSON.parse(body).Language}`);
+                console.log(`\nPlot: ${JSON.parse(body).Plot}`);
+                console.log(`\nActors: ${JSON.parse(body).Actors}`);
+                console.log("\nIf you haven't watched 'Mr. Nobody,' then you should: <http://www.imdb.com/title/tt0485947/>");
+                console.log("It's on Netflix!");
+            }
+            else {
+                console.log('error:', error); // Print the error if one occurred
+                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                console.log('body:', body); // Print the HTML for the Google homepage.
+            }
+        });
+    }
+
+    else {
+            // Then run a request to the OMDB API with the movie specified
+
+            request(`http://www.omdbapi.com/?t=${term}&y=&plot=short&apikey=${process.env.OMDB_ID}`, function (error, response, body) {
+
+                // If the request is successful (i.e. if the response status code is 200)
+                if (!error && response.statusCode === 200) {
+
+                    console.log(`\nTitle of movie: ${JSON.parse(body).Title}`);
+                    console.log(`\nRelease Year: ${JSON.parse(body).Year}`);
+                    console.log(`\nIMDB Rating: ${JSON.parse(body).Ratings[0].Value}`);
+                    console.log(`\nRotten Tomatoes Rating: ${JSON.parse(body).Ratings[2].Value}`);
+                    console.log(`\nCountry where produced: ${JSON.parse(body).Country}`);
+                    console.log(`\nMovie Language: ${JSON.parse(body).Language}`);
+                    console.log(`\nPlot: ${JSON.parse(body).Plot}`);
+                    console.log(`\nActors: ${JSON.parse(body).Actors}`);
+                }
+                else {
+                    console.log('error:', error); // Print the error if one occurred
+                    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                    console.log('body:', body); // Print the HTML for the Google homepage.
+                }
+
+
+            })
+        };
+
+        function randomDo() {
+
+        }
+    };
 
